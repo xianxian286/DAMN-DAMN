626cdd89b5d643e270874d1c5065b93a88674563
fuzihaolizihao <fzhfzh102030@sina.com>
2024-04-30 13:48:16 +0800

    随机点名+分组

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const App = () => {
             path: "/callroll",
             element:<Callroll/>
           },
+          
       ]);
     return ( <RouterProvider router={router} />)
 }
--- src/callroll.js
+++ src/callroll.js
@@ -1,13 +1,61 @@
 import './App.css';
 import { useEffect, useState, React } from 'react';
 import axios from 'axios';
-import { List, Descriptions, Rating, Button, ButtonGroup, Modal, Text } from '@douyinfe/semi-ui';
+import { List, Descriptions, Rating, Button, ButtonGroup, Modal, Text, Spin, Tabs, TabPane } from '@douyinfe/semi-ui';
+import useSWR from 'swr';
 
+const style = {
+    border: '1px solid var(--semi-color-border)',
+    backgroundColor: 'var(--semi-color-bg-2)',
+    borderRadius: '3px',
+    paddingLeft: '20px',
+    margin: '8px 2px',
+};
+
+function Student({ students, selectedStudent, isNotGroup }){
+    return (
+        <List
+            grid={{
+                gutter: 12,
+                xs: 0,
+                sm: 0,
+                md: 12,
+                lg: 8,
+                xl: 8,
+                xxl: 6,
+            }}
+            bordered
+            dataSource={students}
+            renderItem={item => (
+                <List.Item style={style}>
+                    <div>
+                        <Button disabled={isNotGroup && selectedStudent.some(s => s.id === item.id)}>{item.name}</Button>
+                        <Descriptions
+                            align="center"
+                            size="small"
+                            row
+                            data={[
+                                { key: 'points', value: item.points },
+                                // { key: '反馈数', value: item.feedbacks },
+                            ]}
+                        />
+                        <div style={{ margin: '12px 0', display: 'flex', justifyContent: 'flex-end' }}>
+                            <ButtonGroup theme="borderless" style={{ marginTop: 8 }}>
+                                <Button>编辑</Button>
+                                <Button>更多</Button>
+                            </ButtonGroup>
+                        </div>
+                    </div>
+                </List.Item>
+            )}
+        />
+    )
+}
 
 const Callroll = () => {
-    let [students, setStudents] = useState();
     let [chooseList, setChooseList] = useState();
     const [visible, setVisible] = useState(false);
+    let [selectedStudent, setSelect] = useState([]);
     let [choosenOne, setChosenOne] = useState();
 
     const showDialog = () => {
@@ -15,89 +63,66 @@ const Callroll = () => {
     };
     const handleOk = () => {
         setVisible(false);
+        setSelect([...selectedStudent, choosenOne])
+        setChosenOne()
         console.log('Ok button clicked');
     };
     const handleCancel = () => {
         setVisible(false);
+        setSelect([...selectedStudent, choosenOne])
+        setChosenOne()
         console.log('Cancel button clicked');
     };
     const handleAfterClose = () => {
         console.log('After Close callback executed');
     };
-    useEffect(() => {
-        axios
-          .get('http://localhost:3000/students')
-          .then(response => {
+    const { data:students, error, isLoading} = useSWR('http://localhost:4000/students', url =>
+        axios.get('http://localhost:4000/students').then(response => {
             console.log(response);
             console.log(response.data);
-            setStudents(response.data);
-            setChooseList(response.data);
+            return response.data;
           })
           .catch(error => {
             console.error(error);
           })
+    )
+    
+    useEffect( () => {
+        if(students && selectedStudent.length == students.length){
+            setSelect([])
+        }
+    }, [selectedStudent])
 
-    },[]);
-
-    function CallrollAction(chooseList) {
-        let chooseState = Math.random();
-        setChooseList(chooseList.filter((content, index) => index != parseInt(chooseState / (1 / chooseList.length))))
-        const outputName = new Promise((resolve, reject) => {
-            if(!chooseList || chooseList.length === 0) reject("None")
-            else {
-                showDialog()
-                console.log(choosenOne)
-                resolve(chooseList[parseInt(chooseState / (1 / chooseList.length))].name)
-            }
-        })
-            .then(name => setChosenOne(name)).catch(name => setChosenOne(name))
+    function CallrollAction() {
+        const unselectStudents = students.filter(
+            dataItem => !selectedStudent.some(s => s.id === dataItem.id),
+          );
+        let randomIndex = Math.floor(Math.random() * unselectStudents.length);
+        let randomPerson = unselectStudents[randomIndex];
+        setChosenOne(randomPerson)
+        showDialog()
     }
 
-    const style = {
-        border: '1px solid var(--semi-color-border)',
-        backgroundColor: 'var(--semi-color-bg-2)',
-        borderRadius: '3px',
-        paddingLeft: '20px',
-        margin: '8px 2px',
-    };
- 
+
+    if (isLoading) {
+        return <Spin />;
+      }
+      if (error) {
+        return <div>{error.message}</div>;
+      }
+    
+    
     return (
         <div>
-            <List
-                grid={{
-                    gutter: 12,
-                    xs: 0,
-                    sm: 0,
-                    md: 12,
-                    lg: 8,
-                    xl: 8,
-                    xxl: 6,
-                }}
-                dataSource={students}
-                renderItem={item => (
-                    <List.Item style={style}>
-                        <div>
-                            <h3 style={{ color: 'var(--semi-color-text-0)', fontWeight: 500 }}>{item.name}</h3>
-                            <Descriptions
-                                align="center"
-                                size="small"
-                                row
-                                data={[
-                                    { key: 'points', value: item.points },
-                                    // { key: '反馈数', value: item.feedbacks },
-                                ]}
-                            />
-                            <div style={{ margin: '12px 0', display: 'flex', justifyContent: 'flex-end' }}>
-                                <ButtonGroup theme="borderless" style={{ marginTop: 8 }}>
-                                    <Button>编辑</Button>
-                                    <Button>更多</Button>
-                                </ButtonGroup>
-                            </div>
-                        </div>
-                    </List.Item>
-                )}
-            />
-            <Button onClick={() => {setChosenOne(CallrollAction(chooseList))}}>打开弹窗</Button>
+            <Tabs type="line">
+                <TabPane tab="点名" itemKey="1">
+                    <Student students={students} selectedStudent={selectedStudent} isNotGroup={true}></Student>
+                </TabPane>
+                <TabPane tab="分组" itemKey="2">
+                    <Student students={students} selectedStudent={selectedStudent}></Student>
+                </TabPane>
+            </Tabs>
+            <Button onClick={CallrollAction}>打开弹窗</Button>
             <Modal
                 title="幸运儿"
                 visible={visible}
@@ -106,11 +131,11 @@ const Callroll = () => {
                 onCancel={handleCancel}
                 closeOnEsc={true}
                 okButtonProps={{ size: 'small', type: 'warning' }}
-                cancelButtonProps={{ size: 'small', disabled: true }}
+                cancelButtonProps={{ size: 'small', disabled: false }}
                 okText={'Sounds great!'}
                 cancelText={'No, thanks.'}
             >
-                {choosenOne}
+                {choosenOne?.name}
             </Modal>
         </div>
     );
